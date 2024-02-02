1b1710cd4ff40c7edb6d2f0a330122a4c99e7eb5
Maymiro11 <143181180+Maymiro11@users.noreply.github.com>
2024-02-02 21:23:42 +0100

    styles

---
+++ src/main.js
@@ -0,0 +1,25 @@
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+}
+
+function searchImg() {
+    const BASE_URL = '';
+    const PARAMS = '';
+    const url = BASE_URL + PARAMS;
+
+    const options = {
+        headers: {
+            'X-RapidAPI-Key': '42172991-e7a3268a8ccb87dfba8d5efbc',
+            'X-RapidAPI-Host': ''
+        }
+    }
+}
+
+
+
